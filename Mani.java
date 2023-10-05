63559dfab9913908c26c971d9dbd913b93a3be14
noelbekele <77620326+noelbekele@users.noreply.github.com>
2023-10-05 20:29:28 +0300

    Add files via upload

---
+++ Mani.java
@@ -0,0 +1,132 @@
+package gg;
+import java.util.Scanner; 
+//import javax.swing.JOptionPane;
+public class Mani {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//sysout ctr +space shourt cut 
+		/*	
+		System.out.println("I love mom");
+		System.out.println("I love dad\n");// \n is new line
+		System.out.println("\tI love dad");// \t to put space in the bigaenin 
+		System.out.println("\"I love dad\"");
+		System.out.println("\\I love dad\\");
+		System.out.println();
+		int x; //declaration
+		x=7; //assignment
+		int y=6;//inntiolathion
+		System.out.println(x);
+		System.out.println("this y= "+y+" is the seconde value");
+		float p=3.14f;
+		System.out.println(p);
+		double yp=3.145;
+		System.out.println(yp);
+		boolean tf = true;
+		System.out.println(tf);
+		char sm ='@';
+		System.out.println(sm);
+		String name = "bro";
+		System.out.println("hi "+name);
+	    System.out.println("------------");
+	    System.out.println("------------");
+	    System.out.println("------------");
+	    System.out.println("------------");
+
+		String a ="water";
+		String b ="joli";
+		
+		String temp;
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		
+		System.out.println("------------");
+	    System.out.println("-----3-----");
+	    System.out.println("-----3------");
+	    System.out.println("------------");
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    System.out.println("What is your name? ");
+	    String namee = scanner.nextLine();
+	    System.out.println("how old are you?");
+	    int age = scanner.nextInt();
+	    scanner.nextLine();
+	    System.out.println("what is our fiv.. food? ");
+	    String food =scanner.nextLine();
+		System.out.println("wazaaa "+namee);
+		System.out.println("you'r age is "+age);
+		System.out.println("you'r fiv.. food is"+food);
+		System.out.println("------------");
+	    System.out.println("-----4-----");
+	    System.out.println("-----4------");
+	    System.out.println("------------");
+		int frinds =10;
+		frinds= frinds*2;
+		frinds++;
+		frinds--;
+		System.out.println("------------");
+	    System.out.println("-----5-----");
+	    System.out.println("-----5------");
+	    System.out.println("------------");
+	    String nameee =JOptionPane.showInputDialog("ENTER YOU'R NAME");
+	    JOptionPane.showMessageDialog(null,"Hello "+nameee);
+	    
+	    
+	    int agee= Integer.parseInt(JOptionPane.showInputDialog("ENTER YOU'R age"));
+	    JOptionPane.showMessageDialog(null,"You'r age is "+agee+" years old.");
+	    
+	    double height= Double.parseDouble(JOptionPane.showInputDialog("ENTER YOU'R HEIGHT"));
+	    JOptionPane.showMessageDialog(null,"Your height is "+height+"cm tall");
+	    
+	    System.out.println("------------");
+	    System.out.println("-----6------");
+	    System.out.println("-----6------");
+	    System.out.println("------------");
+	    
+	    double x=3.14;
+	    double y=10;
+	    
+	    double z = Math.max(x, y);
+	    
+	    System.out.println(z);
+	    double a = Math.min(x, y);
+	    System.out.println(a);
+	    double b = Math.abs(y);
+	    System.out.println(b);
+	    double c = Math.sqrt(y);
+	    System.out.println(c);
+	    double d = Math.round(x);
+	    System.out.println(d);
+	    double e = Math.ceil(x);
+	    System.out.println(e);
+	    double f = Math.ceil(x);
+	    System.out.println(f);
+	    
+	    double x;
+	    double y;
+	    double z;
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    
+	    System.out.println("enter side x: ");
+	    x = scanner.nextDouble();
+	    System.out.println("enter side y: ");
+	    y = scanner.nextDouble();
+	    z= Math.sqrt((x*x)+(y*y));
+	    System.out.println("The hypotenuse is :"+z);
+	    scanner.close();
+	    */
+		System.out.println("------------");
+	    System.out.println("-----7------");
+	    System.out.println("-----7------");
+	    System.out.println("------------");
+	}
+
+}
+ 
